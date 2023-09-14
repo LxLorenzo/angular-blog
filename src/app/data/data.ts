23b8b9c809ca2ff91b4a4f85d0c99c749befd288
Lorenzo Casa <lorenzocasa1235@gmail.com>
2023-09-14 14:35:23 -0300

    feat: dynamic data to routes

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,14 +1,26 @@
 export const data = [
   {
-    "id": 1,
+    "id": "1",
     "photo": "https://picsum.photos/seed/picsum/600/400",
-    "title": "Lorem ipsum dolor sit amet",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl sed aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam ni"
+    "title": "Artigo 1",
+    "description": "Artigo 1, consectetur adipiscing elit. Nulla euismod, nisl sed aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam ni"
   },
   {
-    "id": 2,
+    "id": "2",
     "photo": "https://picsum.photos/seed/picsum/600/400",
-    "title": "Lorem ipsum dolor sit amet",
-    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl sed aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam ni"
+    "title": "Artigo 2",
+    "description": "Artigo 2, consectetur adipiscing elit. Nulla euismod, nisl sed aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam ni"
+  },
+  {
+    "id": "3",
+    "photo": "https://picsum.photos/seed/picsum/600/400",
+    "title": "Artigo 3",
+    "description": "Artigo 3, consectetur adipiscing elit. Nulla euismod, nisl sed aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam ni"
+  },
+  {
+    "id": "4",
+    "photo": "https://picsum.photos/seed/picsum/600/400",
+    "title": "Artigo 4",
+    "description": "Artigo 4, consectetur adipiscing elit. Nulla euismod, nisl sed aliquam ultricies, nunc nisl aliquet nunc, vitae aliquam ni"
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { data } from 'src/app/data/data';
 
 @Component({
   selector: 'app-content',
@@ -6,7 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
-  photoCover:string = "https://picsum.photos/seed/picsum/600/400"
+  photoCover:string = ""
   contentTitle:string = ""
   contentDescription:string = ""
+  private id: string | null = '0';
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get('id')
+    })
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = data.filter((article) => article.id === id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photo
+  }
 }
